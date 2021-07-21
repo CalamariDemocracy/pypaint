2d349030576f5f1804b44b61df4027cf09715ac7
CalamariDemocracy <welfortnitew97@gmail.com>
2021-07-21 19:24:11 -0400

    Add basic draw functionality

--- pypaint.py
+++ pypaint.py
@@ -28,13 +28,12 @@ def draw_line(surface, start_pos, end_pos, color, width=1):
         pygame.draw.circle(surface, color, (x, y), math.ceil(width / 2))
 
 
-def draw_lines(surface, lines, color, width=1):
-    for line in lines:
-        for j, point in enumerate(line):
-            if j < 1:
-                pygame.draw.circle(surface, color, point, math.ceil(width / 2))
-                continue
-            draw_line(surface, line[j - 1], point, color)
+def draw_points(surface, points, color, width=1):
+    for j, point in enumerate(points):
+        if j < 1:
+            pygame.draw.circle(surface, color, point, math.ceil(width / 2))
+            continue
+        draw_line(surface, points[j - 1], point, color)
 
 
 def get_delta_time(ms_last_frame):
@@ -44,13 +43,31 @@ def get_delta_time(ms_last_frame):
 def main():
     screen = pygame.display.set_mode(DEFAULT_SIZE)
     clock = pygame.time.Clock()
+    pen = Pen()
+    lines = []
+    lines_info = []
+
     while True:
         ms = clock.tick(FPS)
         dt = get_delta_time(ms)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button != 2:
+                    lines.append([])
+                    lines_info.append({'color': pen.color})
+                    pen.down = True
+            if event.type == pygame.MOUSEBUTTONUP:
+                if event.button != 2:
+                    pen.down = False
+        if pen.down:
+            point = pygame.mouse.get_pos()
+            if point not in lines[-1]:
+                lines[-1].append(point)
         screen.fill('white')
+        for i, line in enumerate(lines):
+            draw_points(screen, line, lines_info[i]['color'])
         pygame.display.update()
 
 
