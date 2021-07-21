337d71eed999c65569f0b9780b9286597897344f
CalamariDemocracy <welfortnitew97@gmail.com>
2021-07-21 18:50:37 -0400

    Add draw_line function

--- pypaint.py
+++ pypaint.py
@@ -1,3 +1,5 @@
+import math
+
 import pygame
 
 
@@ -6,6 +8,16 @@ DEFAULT_HEIGHT = 600
 DEFAULT_SIZE = (DEFAULT_WIDTH, DEFAULT_HEIGHT)
 
 FPS = 60
+
+
+def draw_line(surface, start_pos, end_pos, color, width=1):
+    dx = end_pos[0] - start_pos[0]
+    dy = end_pos[1] - start_pos[1]
+    distance = max(abs(dx), abs(dy))
+    for pixel in range(distance):
+        x = start_pos[0] + pixel/distance*dx
+        y = start_pos[1] + pixel/distance*dy
+        pygame.draw.circle(surface, color, (x, y), math.ceil(width / 2))
 
 
 def main():
