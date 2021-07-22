7002d6fcba39e42cf517ef7cd56592e30a9edc48
CalamariDemocracy <welfortnitew97@gmail.com>
2021-07-22 14:23:25 -0400

    Add scuffed erase functionalilty

--- pypaint.py
+++ pypaint.py
@@ -19,6 +19,13 @@ class Pen:
         self.down = False
 
 
+class Eraser:
+    def __init__(self, canvas_color=(255, 255, 255), pos=(0, 0), width=1):
+        self.pos = pygame.Vector2(pos)
+        self.width = width
+        self.color = canvas_color
+
+
 def draw_line(surface, start_pos, end_pos, color, width=1):
     """draws circles pixel by pixel between two points to create a line"""
     dx = end_pos[0] - start_pos[0]
@@ -52,8 +59,11 @@ def main():
     screen = pygame.display.set_mode(DEFAULT_SIZE)
     clock = pygame.time.Clock()
     pen = Pen()
+    eraser = Eraser()
+    selected_tool = pen
     range_width = (1, 25)
     lines = []
+    erase_marks = []
 
     while True:
         ms = clock.tick(FPS)
@@ -65,15 +75,22 @@ def main():
                 if event.key == pygame.K_BACKSPACE:
                     if pygame.key.get_mods() and pygame.KMOD_CTRL:
                         lines.clear()
+                        erase_marks.clear()
                     elif lines:
                         lines.pop()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button != 2:
-                    lines.append({'color': pen.color, 'width': pen.width, 'points': []})
                     pen.down = True
+                if event.button == 1:
+                    lines.append({'color': pen.color, 'width': pen.width, 'points': []})
+                if event.button == 3:
+                    selected_tool = eraser
+                    erase_marks.append({'width': pen.width, 'points': []})
             if event.type == pygame.MOUSEBUTTONUP:
                 if event.button != 2:
                     pen.down = False
+                if event.button == 3:
+                    selected_tool = pen
             if event.type == pygame.MOUSEWHEEL:
                 pen.width += -1 if event.y > 0 else 1
                 if pen.width > max(range_width):
@@ -82,11 +99,18 @@ def main():
                     pen.width = min(range_width)
         if pen.down:
             point = pygame.mouse.get_pos()
-            if point not in lines[-1]['points']:
-                lines[-1]['points'].append(point)
+            if selected_tool == pen:
+                if point not in lines[-1]['points']:
+                    lines[-1]['points'].append(point)
+            if selected_tool == eraser:
+                if point not in erase_marks[-1]['points']:
+                    erase_marks[-1]['points'].append(point)
+                    
         screen.fill('white')
-        for i, line in enumerate(lines):
+        for line in lines:
             draw_points(screen, line['points'], line['color'], line['width'])
+        for mark in erase_marks:
+            draw_points(screen, mark['points'], eraser.color, mark['width'])
         pygame.display.update()
 
 
